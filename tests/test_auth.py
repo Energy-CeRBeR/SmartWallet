3e9db017d68359922fe56234f47a7d5f8014c34d
Igor Chesnykh <igor.chesnyx@mail.ru>
2023-12-24 11:34:44 +0300

    Commit to delete errors on notebook

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,12 +5,16 @@ from tests.conftest import client, async_session_maker
 
 def test_register():
     response = client.post("/auth/register", json={
-            "email": "string",
-            "password": "string",
-            "is_active": True,
-            "is_superuser": False,
-            "is_verified": False,
-            "username": "string"
+        "email": "string",
+        "password": "string",
+        "is_active": True,
+        "is_superuser": False,
+        "is_verified": False,
+        "username": "string"
     })
 
     assert response.status_code == 201
+
+
+def temp_test():
+    ...
