6705a3dd0dc26faa1647f414c55cfb0d2a4a4884
Igor Chesnykh <igor.chesnyx@mail.ru>
2024-04-25 13:33:41 +0300

    Create auth utils

--- config_data/config.py
+++ config_data/config.py
@@ -1,5 +1,11 @@
 from dataclasses import dataclass
+from pathlib import Path
+
 from environs import Env
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).parent.parent
 
 
 @dataclass
@@ -7,9 +13,23 @@ class DataBase:
     URL: str
 
 
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "auth_upd" / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "auth_upd" / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 15
+
+
 @dataclass
 class Config:
     database: DataBase
+
+
+class Settings(BaseSettings):
+    auth_jwt: AuthJWT = AuthJWT()
+
+
+settings = Settings()
 
 
 def load_config(path: str | None = None) -> Config:
